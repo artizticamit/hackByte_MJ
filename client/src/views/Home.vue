19ecfe402bc3e3e4db9eb010e25b957ba83d7715
mayankjx <prototype.mayank@gmail.com>
2022-04-15 16:59:33 +0530

    addedd home page

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,59 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 class="headng">Natural Language Understanding</h1>
+    <form>
+      <textarea
+        class="input"
+        name=""
+        id=""
+        cols="30"
+        rows="10"
+        v-model="inputText"
+      ></textarea>
+    </form>
+    <button class="btnStyle1">Check</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: {},
+  data() {
+    return {
+      inputText: "",
+    };
+  },
+};
 </script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Special+Elite&display=swap");
+
+.home {
+  font-family: "Special Elite", cursive;
+  color: black;
+}
+
+.input {
+  font-family: "Special Elite";
+  border: none;
+  background-color: whitesmoke;
+  width: 70%;
+  height: 300px;
+  font-size: 20px;
+  outline: none;
+  padding: 15px 10px;
+}
+
+.btnStyle1 {
+  font-size: 30px;
+  margin: 20px 0px;
+  border: none;
+  background: wheat;
+  border-radius: 10px;
+  padding: 10px 15px;
+}
+</style>
